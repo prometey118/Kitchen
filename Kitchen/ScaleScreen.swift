81f272a0aa1e3ec59cae95bf5c2b05168394beab
Yaroslav <neptun2001yarik@gmail.com>
2023-07-04 15:54:39 +0300

    first view

---
+++ Kitchen/ScaleScreen.swift
@@ -0,0 +1,28 @@
+//
+//  ScaleScreen.swift
+//  Kitchen
+//
+//  Created by Yaroslav Savin on 04.07.2023.
+//
+
+import Foundation
+import UIKit
+public class Size {
+    public func scaleWidth(_ value : CGFloat) -> CGFloat{
+        let x = UIScreen.main.bounds.width/375
+        return value * x
+    }
+
+    public func scaleHeight(_ value : CGFloat) -> CGFloat{
+        let y = UIScreen.main.bounds.height/812
+        return value * y
+    }
+    
+    public func screenWidth() -> CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    public func screenHeight() -> CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+}
