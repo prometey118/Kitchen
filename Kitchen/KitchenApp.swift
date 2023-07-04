af7d3884084216c7fb9a278d55447233a06090f7
Yaroslav <neptun2001yarik@gmail.com>
2023-07-04 09:51:28 +0300

    Initial Commit

---
+++ Kitchen/KitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenApp.swift
+//  Kitchen
+//
+//  Created by Yaroslav Savin on 04.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct KitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
