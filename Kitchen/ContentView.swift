6de596ec16c1dae3abe4ba806f7500cc05269610
Yaroslav <neptun2001yarik@gmail.com>
2023-07-04 17:02:34 +0300

    first screen

--- Kitchen/ContentView.swift
+++ Kitchen/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Hui()
-            Hui()
+        VStack(spacing: 20) {
+            TextOnImage(text: "Пекарни и кондитерские", url: url)
+            TextOnImage(text: "Фастфуд", url: url2)
+            TextOnImage(text: "Азиатская кухня", url: url3)
+            TextOnImage(text: "Супы", url: url4)
         }
     }
     
@@ -19,32 +21,31 @@ struct ContentView: View {
 
 
 
-struct Hui: View {
+
+struct TextOnImage: View {
     var size = Size()
+    var text: String
+    var url: String
     var body: some View {
         ZStack {
-            VStack {
-//                Image(systemName: "globe")
-//                    .
-//                    .foregroundColor(.accentColor)
-                Text("awc").frame(maxWidth: 343, maxHeight: 148)
-                    .background(AsyncImage(url: URL(string: url),
-                                           content: { image in
-                                                   image.resizable()
-                                                       .aspectRatio(contentMode: .fit)
-                                               },
-                                               placeholder: {
-                                                   ProgressView()
-                                               }))
-                
-                
-                
-                
-                
-            }
+//            Color.red.ignoresSafeArea()
+            AsyncImage(url: URL(string: url),
+                                   content: { image in
+                                           image.resizable()
+                                               .aspectRatio(contentMode: .fit)
+                                       },
+                                       placeholder: {
+                                           ProgressView()
+            }).frame(width: size.scaleWidth(434), height: size.scaleHeight(148))
+            Text(text)
+                .font(.system(size: 20, weight: .medium))
+                .frame(width: size.scaleWidth(130), height: size.scaleHeight(100))
+                .border(Color(.red))
+                .offset(x: -100, y: -40)
+            
         }
-        .cornerRadius(20)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
