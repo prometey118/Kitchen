37c8cae4ae6cc12545afa8cbbb99db39c79291e0
Yaroslav <neptun2001yarik@gmail.com>
2023-07-04 11:09:26 +0300

    TEst

---
+++ Kitchen/Kitchens.swift
@@ -0,0 +1,12 @@
+//
+//  Kitchens.swift
+//  Kitchen
+//
+//  Created by Yaroslav Savin on 04.07.2023.
+//
+
+import Foundation
+struct сategories: Decodable {
+    var name:String
+    var image_url: String
+}
