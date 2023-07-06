03ca2f56e67892056810772677edfdb456b9d70d
Yaroslav <neptun2001yarik@gmail.com>
2023-07-06 14:56:21 +0300

    switch view

--- Kitchen/CategoriesView.swift
+++ Kitchen/CategoriesView.swift
@@ -6,10 +6,68 @@
 //
 
 import SwiftUI
-
+fileprivate var viewTemp = 0
 struct CategoriesView: View {
     var size = Size()
-    let data = (1...10).map { "Item \($0)" }
+    @State var mainTemp = 0
+    let data = (1...50).map { "Item \($0)" }
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+            ]
+    var body: some View {
+        VStack{
+            Buttons(mainTemp: $mainTemp)
+            switch  mainTemp {
+            case 0:
+                MainCategories()
+            case 1:
+                MainView()
+            case 3:
+                MainCategories()
+            default:
+                MainCategories()
+            }
+        }
+        
+        
+        }
+    }
+
+struct Buttons: View {
+    @Binding var mainTemp: Int
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: true) {
+            HStack{
+                Button("Все меню") {
+                    mainTemp = 0
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Item 2") {
+                    mainTemp = 1
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Все меню") {
+                    viewTemp = 2
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Все меню") {
+                    viewTemp = 3
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            .padding(5)
+            .padding(.top)
+        }
+    }
+
+    
+}
+
+struct MainCategories: View {
+    var size = Size()
+    let data = (1...50).map { "Item \($0)" }
     let columns = [
             GridItem(.flexible()),
             GridItem(.flexible()),
@@ -17,46 +75,39 @@ struct CategoriesView: View {
             ]
     var body: some View {
         VStack {
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack{
-                    Button("Все меню", action: pr)
-                        .buttonStyle(.borderedProminent)
-                    Button("Все меню", action: pr)
-                        .buttonStyle(.borderedProminent)
-                    Button("Все меню", action: pr)
-                        .buttonStyle(.borderedProminent)
-                    Button("Все меню", action: pr)
-                        .buttonStyle(.borderedProminent)
-                }
-                .padding(5)
-            }
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(data, id: \.self) { item in
-                    VStack(alignment: .center){
-                        
-                        ZStack{
-                            Rectangle()
-                                .frame(width: 100, height: 100)
-                                Color(red: 0.97, green: 0.97, blue: 0.96)
-                            AsyncImage(url: URL(string: url5),
-                                                   content: { image in
-                                                           image.resizable()
-                                    .offset(x: 4,y:9)
-                                    .aspectRatio(contentMode: .fill)
-    //                                .resizable()
-                                                        .scaledToFit()                                                     
-                                                       },
-                                                       placeholder: {
-                                                           ProgressView()
-                            }).frame(width: 100, height: 100)
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(data, id: \.self) { item in
+                            VStack(alignment: .center){
+                                
+                                ZStack{
+                                    Rectangle()
+                                        .frame(width: 100, height: 100)
+                                    Color(red: 0.97, green: 0.97, blue: 0.96)
+                                    AsyncImage(url: URL(string: url5),
+                                               content: { image in
+                                        image.resizable()
+                                            .offset(x: 4,y:9)
+                                            .aspectRatio(contentMode: .fill)
+                                        //                                .resizable()
+                                            .scaledToFit()
+                                    },
+                                               placeholder: {
+                                        ProgressView()
+                                    }).frame(width: 110, height: 110)
+                                    
+                                }.cornerRadius(10)
+                                    .padding()
+                                Text(item)
+                                
+                                
+                            }
                             
-                        }.cornerRadius(20)
-                        .padding()
-                        Text(item)
-                        
                         
                     }
-                    
                 }
             }
         }
@@ -68,6 +119,4 @@ struct KitchenView_Previews: PreviewProvider {
         CategoriesView()
     }
 }
-func pr() {
-    print("sss")
-}
+
